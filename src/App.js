bd6808f53f19cd2cc7b3615cbbd2e0619cf52259
FERHATSUMER <sumerf39@gmail.com>
2023-03-23 23:53:16 +0300

    birthday-react

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <main>
       <section className='container' >
-      <h3> {people.length} birthday today</h3>
+      <h3> {people.length} Birthday Today</h3>
       <List  people={people}
       />
       <button onClick={()=>setPeople([]) }>
-    clear all
+    Clear All
       </button>
       </section>
     </main>
